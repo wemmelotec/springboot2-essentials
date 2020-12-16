3b32de5cad5d933d8d604743da847b373a0ec7a7
wemmelotec <tonmelo@hotmail.com>
2020-12-16 14:39:55 -0300

    end of class 13 spring boot 2 essentials

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -2,11 +2,21 @@ package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 //por estar utilizando o lombok eu não preciso criar get e setters, equals, hashcode, to string basta anotar a classe com o Date
 @Data
 @AllArgsConstructor //para gerar um construtor com todos esse valores
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 /*
