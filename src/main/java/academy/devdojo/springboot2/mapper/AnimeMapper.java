8976ad5cb9464381f0b3fb6a0d3da265588cd31b
wemmelotec <wellington.magno@gmail.com>
2021-02-27 07:57:56 -0300

    end of class mapstruct

--- src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/springboot2/mapper/AnimeMapper.java
@@ -6,11 +6,15 @@ import academy.devdojo.springboot2.request.AnimePutRequestBody;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+/*
+ela está sendo utilizada primeiro no service (save, replace)
+ */
+@Mapper(componentModel = "spring")//essa notação me possibilita fazer injeção de dependência
 public abstract class AnimeMapper {
-
+    //este método foi implementado para que eu consiga chamar os dois métodos abstracts abaixo
     public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
 
+    //esses dois métodos convertem AnimePostRequestBody e o AnimePutRequestBody para Anime
     public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
     public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
 }
